b9422372e96af10abdbe910fe16fb0fd2a4b524f
psurac <philipsurac@web.de>
2023-08-12 21:05:30 +0300

    Added logo to Footer.js in little-lemon_capstone

--- little-lemon_capstone/little-lemon/src/Footer.js
+++ little-lemon_capstone/little-lemon/src/Footer.js
@@ -1,8 +1,12 @@
+import Logo from './Logo.svg';
 import Nav from './Nav.js';
 
 function Footer() {
     return (
         <footer>
+            <div className="logo">
+                <img src={Logo} alt="Little Lemon Logo" />
+            </div>
             <div className="footer">
                 <Nav />
             </div>
