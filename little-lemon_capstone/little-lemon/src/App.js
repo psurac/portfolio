1268fa06bf51c41b3c07a11bf0af1749bd388457
psurac <philipsurac@web.de>
2023-08-14 00:00:20 +0300

    Adding grid and text styling in App.css

--- little-lemon_capstone/little-lemon/src/App.js
+++ little-lemon_capstone/little-lemon/src/App.js
@@ -5,11 +5,11 @@ import Footer from './Footer';
 
 function App() {
   return (
-    <>
+    <div className="App">
       <Header />
       <Main />
       <Footer />
-    </>
+    </div>
   );
 }
 
