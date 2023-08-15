c5a3f343d904bba3379df5b1c58a9277858ab978
psurac <philipsurac@web.de>
2023-08-15 15:18:48 +0300

    Added css for HeroSection, and some minor changes ll_capstone

--- little-lemon_capstone/little-lemon/src/components/HeroSection.js
+++ little-lemon_capstone/little-lemon/src/components/HeroSection.js
@@ -1,15 +1,16 @@
 import restauranfood from '../pictures/restauranfood.jpg';
+import './section.css';
 
 function HeroSection() {
     return (
-        <div className="flex-container">
+        <div className="prim1 padd-right-left hero-flex">
             <div className="text">
                 <h1 className="title">Little Lemon</h1>
                 <h2 className="subtitle">Chicago</h2>
-                <p className="leadtext"></p>
+                <p className="leadtext">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
             </div>
             <div className="image">
-                <img src={restauranfood} alt="Food served in Little-Lemon" width={200} />
+                <img id='hero-image' src={restauranfood} alt="Food served in Little-Lemon" />
             </div>
         </div>
     );
