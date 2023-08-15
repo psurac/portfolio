8024d46f60b993d3bea3aa81d48714a6bb94f85e
psurac <philipsurac@web.de>
2023-08-15 21:57:51 +0300

    Added some styling features to the Button in ll_capstone

--- little-lemon_capstone/little-lemon/src/components/Button.js
+++ little-lemon_capstone/little-lemon/src/components/Button.js
@@ -1,11 +1,7 @@
 import { Link } from "react-router-dom";
 
 function Button( { link, buttonText }) {
-    return (
-        <div className="lemonButton highlighttext">
-            <Link to={link}>{buttonText}</Link>
-        </div>
-    );
+    return <Link to={link} className="lemonButton highlighttext">{buttonText}</Link>;
 };
 
 export default Button;
