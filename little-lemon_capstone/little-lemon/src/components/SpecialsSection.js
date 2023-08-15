42f863b293621db96b8d873fa436c9beeb9a5ec6
psurac <philipsurac@web.de>
2023-08-15 22:43:12 +0300

    specialsection started in ll_capstone

--- little-lemon_capstone/little-lemon/src/components/SpecialsSection.js
+++ little-lemon_capstone/little-lemon/src/components/SpecialsSection.js
@@ -1,7 +1,14 @@
+import Button from "./Button.js";
+
 function SpecialsSection() {
+    const link = '/Menu';
+    const buttonText = 'Menu';
+
     return (
-        <div>
-            <h1>Specials Section</h1>
+        <div className="grid-ss">
+            <h1 className="sectiontitle">Specials</h1>
+            <Button link={link} buttonText={buttonText} />
+
         </div>
     );
 };
