397798de228f4b755fa09eb10e087fce65422db4
psurac <philipsurac@web.de>
2023-08-16 22:19:45 +0300

    Gave the specials section some styling

--- little-lemon_capstone/little-lemon/src/components/SpecialsSection.js
+++ little-lemon_capstone/little-lemon/src/components/SpecialsSection.js
@@ -1,14 +1,17 @@
 import Button from "./Button.js";
+import { useSitesContext } from '../context/SitesContext.js';
 
 function SpecialsSection() {
-    const link = '/Menu';
-    const buttonText = 'Menu';
+    const name = 'Menu';
+    const link = useSitesContext().find(item => item.name === name).path;
 
     return (
-        <div className="grid-ss">
+        <div className="grid-ss padd-right-left">
             <h1 className="sectiontitle">Specials</h1>
-            <Button link={link} buttonText={buttonText} />
-
+            <Button link={link} buttonText={name} />
+            <div className="specials-container">
+                Specials
+            </div>
         </div>
     );
 };
