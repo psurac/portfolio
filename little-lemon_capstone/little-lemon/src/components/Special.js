32773f75e25971b8a2034140263508ef68ce9921
psurac <philipsurac@web.de>
2023-08-16 23:36:11 +0300

    Started the Special component, and made some changes in styling

--- little-lemon_capstone/little-lemon/src/components/Special.js
+++ little-lemon_capstone/little-lemon/src/components/Special.js
@@ -1,7 +1,10 @@
 function Special({dish}) {
     return (
         <div className="special">
-            <h2>{dish.name}</h2>
+            <img src={dish.image} alt={dish.name} />
+            <h3 className="cardtitle">{dish.name}</h3>
+            <h4 className="highlighttext">{dish.price}</h4>
+            <p className="weeksspecial">{dish.description}</p>
         </div>
     );
 };
