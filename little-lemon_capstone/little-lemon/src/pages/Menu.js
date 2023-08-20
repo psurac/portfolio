512d81e10322e70f634d01b315733b4f381d2d85
psurac <philipsurac@web.de>
2023-08-21 00:16:18 +0300

    Added some content and logic to Meun.js

--- little-lemon_capstone/little-lemon/src/pages/Menu.js
+++ little-lemon_capstone/little-lemon/src/pages/Menu.js
@@ -1,8 +1,13 @@
 import Button from "../components/Button";
 import { useMenu } from "../context/MenuContext";
+import { useSitesContext } from '../context/SitesContext.js';
 
 function Menu() {
     const dishes = useMenu();
+    const name = 'Reservation';
+    const link = useSitesContext().find(item => item.name === name).path;
+    const buttonText = 'Reserv a Table';
+
     const menu = [];
     dishes.forEach((dish) => {
         if (!menu.includes(dish.category)) menu.push(dish.category);
@@ -14,13 +19,16 @@ function Menu() {
             {menu.map((category) => (
                 <div key={category}>
                     <h2>{category}</h2>
-                    <div>
                         {dishes.map((dish) => (
-                            <h4 key={dish.id}>{dish.name}</h4>
+                            <div key={dish.id}>
+                                <h4>{dish.name}</h4>
+                                <p>{dish.description}</p>
+                                <h4>${dish.price}</h4>
+                            </div>
                         ))}
-                    </div>
                 </div>
             ))}
+            <Button link={link} buttonText={buttonText} />
         </div>
     );
 };
