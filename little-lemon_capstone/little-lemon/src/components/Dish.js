e5edf4046bd56d3e2f3d9483f47f5a04b3e1c34c
psurac <philipsurac@web.de>
2023-09-03 00:15:29 +0300

    Styled the Dish and gave the customisation functionality to hide

--- little-lemon_capstone/little-lemon/src/components/Dish.js
+++ little-lemon_capstone/little-lemon/src/components/Dish.js
@@ -1,26 +1,48 @@
+import { useState } from 'react';
 import { useMenu } from "../context/MenuContext";
 import { useShoppingCard } from "../context/ShoppingCardContext";
 import "./Dish.css";
 
 function Dish({ dish }) {
+    const [ toggleCustom, setToggleCustom ] = useState(false);
+    const [ sumDish, setSumDish ] = useState(dish.price);
     const { customs } = useMenu();
     const { addToCard, deleteFromCard } = useShoppingCard();
+
     return (
         <div className="dish-container textsmall">
             <span className="dish-name weeksspecial">{dish.name}</span>
             <span className="dish-price">$ {dish.price}</span>
             <form className="customisation-form" onSubmit={null}>
-                <div className="customisation-container">
+                <div className="customisation-container" style={{display: toggleCustom ? 'block' : 'none'}}>
                     {customs.map(({ ingredient, price }) => (
-                        <label key={ingredient} htmlFor={ingredient}>
-                            <input type="checkbox" id={ingredient} name={ingredient} />
-                            <span id={ingredient}>{ingredient}</span>
-                            <span id={price}>{price}</span>
+                        <label className="custom-flex-container" key={ingredient} htmlFor={ingredient}>
+                            <div>
+                                <input
+                                    type="checkbox"
+                                    id={ingredient}
+                                    name={ingredient}
+                                    onChange={(e) => {
+                                        e.target.checked ?
+                                            setSumDish(Math.round((sumDish + price) * 100) / 100) :
+                                            setSumDish(Math.round((sumDish - price) * 100) / 100)
+                                    }} />
+                                <span id={ingredient}> {ingredient}</span>
+                            </div>
+                            <span id={price}>$ {price}</span>
                         </label>
                     ))}
                 </div>
-                <button className="lemonButton-small" onClick={null}>Customais Dish</button>
+                <button
+                    className="lemonButton-small"
+                    onClick={(e) => {
+                        e.preventDefault();
+                        setToggleCustom(!toggleCustom);
+                }}>
+                    Customais Dish
+                </button>
                 <button className="lemonButton-small" type="submit">Add to order</button>
+                <span className="price-sum">Total: $ {sumDish}</span>
             </form>
         </div>
     )
