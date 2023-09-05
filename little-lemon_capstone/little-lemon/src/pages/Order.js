c0fb98b2be6511a01b99c08b8861fdf5447070a9
psurac <philipsurac@web.de>
2023-09-05 19:45:54 +0300

    Completed the html part of the form Order

--- little-lemon_capstone/little-lemon/src/pages/Order.js
+++ little-lemon_capstone/little-lemon/src/pages/Order.js
@@ -4,18 +4,18 @@ function Order() {
     const { card } = useShoppingCard();
     return (
         <div className="order-container padd-right-left">
-            { Array.isArray(card) && card.length ? card.map((item, index) => (
+            {Array.isArray(card) && card.length ? card.map((item, index) => (
                 <div className="meal" key={index}>
                     <h5 className="dish-name">{item.name}</h5>
                     <h5 className="dish-price">{item.price}</h5>
                     <span className="addings"> Additional:
-                        { Array.isArray(item.customisation) && item.customisation.length ?
-                        item.customisation.map((ingredient, index) =>(
-                            index === 0 ? <> {ingredient.ingredient}</> : <>, {ingredient.ingredient}</>
-                        )) : <> -</>}
+                        {Array.isArray(item.customisation) && item.customisation.length ?
+                            item.customisation.map((ingredient, index) => (
+                                index === 0 ? <> {ingredient.ingredient}</> : <>, {ingredient.ingredient}</>
+                            )) : <> -</>}
                     </span>
                 </div>
-            )) : <span>Nothing selected</span> }
+            )) : <span>Nothing selected</span>}
             <form className="order-form" onSubmit={null}>
                 <label className="check-order-label" htmlFor="check-order">
                     <input type="checkbox" id="check-order" name="check-order" required></input>
@@ -27,6 +27,41 @@ function Order() {
                 <label className="phone-number-label" htmlFor="phone-number">
                     <input type="tel" id="phone-number" name="phone-number" required placeholder="Phone Number"></input>
                 </label>
+                <div className="addrss-from-container">
+                    <label className="street-address-label" htmlFor="street-address">
+                        <span className="street-address-text">Street and house number</span>
+                        <input
+                            type="text"
+                            id="street-address"
+                            name="street-address"
+                            required
+                            placeholder="Street and house number"></input>
+                    </label>
+                    <label className="zip-number-label" htmlFor="zip-number">
+                        <span className="zip-number-text">Zip code</span>
+                        <input
+                            type="text"
+                            id="zip-number"
+                            name="zip-number"
+                            required
+                            placeholder="Zip Code"></input>
+                    </label>
+                    <label className="city-label" htmlFor="city">
+                        <span className="city-text">City</span>
+                        <input
+                            type="text"
+                            id="city"
+                            name="city"
+                            required
+                            placeholder="City"></input>
+                    </label>
+                    <label className="check-newsletter-label" htmlFor="check-newsletter">
+                        <input type="checkbox" id="check-newsletter" name="check-newsletter" required></input>
+                        <span className="check-newsletter-text">
+                            I want to get the newsletter with the latet updates.
+                        </span>
+                    </label>
+                </div>
             </form>
         </div>
     );
