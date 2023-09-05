3cac45a060559dddb25641ce8085ee49a257a203
psurac <philipsurac@web.de>
2023-09-05 16:46:18 +0300

    Added stuff to the from from Order page

--- little-lemon_capstone/little-lemon/src/pages/Order.js
+++ little-lemon_capstone/little-lemon/src/pages/Order.js
@@ -12,12 +12,21 @@ function Order() {
                         { Array.isArray(item.customisation) && item.customisation.length ?
                         item.customisation.map((ingredient, index) =>(
                             index === 0 ? <> {ingredient.ingredient}</> : <>, {ingredient.ingredient}</>
-                        )) : <>-</>}
+                        )) : <> -</>}
                     </span>
                 </div>
             )) : <span>Nothing selected</span> }
             <form className="order-form" onSubmit={null}>
-
+                <label className="check-order-label" htmlFor="check-order">
+                    <input type="checkbox" id="check-order" name="check-order" required></input>
+                    <span className="check-order-text"> I checked the Order and confirm.</span>
+                </label>
+                <label className="email-label" htmlFor="email">
+                    <input type="email" id="email" name="email" required placeholder="Email"></input>
+                </label>
+                <label className="phone-number-label" htmlFor="phone-number">
+                    <input type="tel" id="phone-number" name="phone-number" required placeholder="Phone Number"></input>
+                </label>
             </form>
         </div>
     );
