6ff607b3f7bf1722ea2a236b05b493f48f7be58f
psurac <philipsurac@web.de>
2023-09-08 01:15:17 +0300

    Fixed some styling in Address fragment and added the new UserInfo in Registration page

--- little-lemon_capstone/little-lemon/src/pages/Register.js
+++ little-lemon_capstone/little-lemon/src/pages/Register.js
@@ -1,21 +1,25 @@
 import { useState } from 'react';
 import PhoneAndMail from '../components/fragments/PhoneAndMail';
 import Address from '../components/fragments/Address';
+import UserInformation from '../components/fragments/UserInformation'
 import './Register.css';
 
 function Register() {
     const [ key, setKey ] = useState(1);
+    const [ userInfo, setUserInfo ] = useState();
+    const [ userShow, setUserShow ] = useState();
 
     function submitRegistration(e) {
         e.preventDefault();
-        alert('Check rour Mailbox and fonfirm the registration details');
+        setUserInfo('Please check your Mailbox and confirm!');
+        setUserShow(true);
         setKey(key + 1);
     }
 
     return (
-        <div className="padd-right-left registration-container" key={key}>
+        <div className="padd-right-left registration-container">
             <h3 className="sectiontitle">Registration</h3>
-            <form className="register-form" onSubmit={(e) => submitRegistration(e)}>
+            <form className="register-form" onSubmit={(e) => submitRegistration(e)} key={key}>
                 <PhoneAndMail />
                 <label className="user-password-label" htmlFor="user-password-login">
                     *<input type="password" id="user-password-login" name="user-password-login" placeholder="Password" required></input>
@@ -23,6 +27,7 @@ function Register() {
                 <Address required={false} />
                 <button type="submit" className="lemonButton-small textsmall">Register</button>
             </form>
+            <UserInformation info={userInfo} show={userShow} />
         </div>
     );
 };
